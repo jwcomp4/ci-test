cd0b61f6d54a0b2559890350e05e48e46a755cce
John Compton <johncompton@Johns-MacBook-Air.local>
2023-04-11 16:29:41 -0500

    add super to app.py.

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.layout = ddk.App(
         ddk.Header(
             [
                 ddk.Logo(src=app.get_asset_url("logo.png")),
-                ddk.Title("Dash Enterprise Amazing Sample Application!!"),
+                ddk.Title("Dash Enterprise Super Sample Application!!"),
             ]
         ),
         ddk.Row(
