6b32fbec387e2f9c588f4382771f9c59bdfd5944
John Compton <johncompton@Johns-MacBook-Air.local>
2023-04-11 15:26:41 -0500

    Add exclamation points to title in app.py.

--- app.py
+++ app.py
@@ -7,42 +7,68 @@ server = app.server  # expose server variable for Procfile
 
 df = px.data.stocks()
 
-app.layout = ddk.App([
-    ddk.Header([
-        ddk.Logo(src=app.get_asset_url('logo.png')),
-        ddk.Title('Dash Enterprise Sample Application'),
-    ]),
-    ddk.Row(children=[
-        ddk.Card(children=[
-            ddk.CardHeader(children=[
-                dcc.Dropdown(
-                    id='title-dropdown',
-                    options=[{'label': i, 'value': i}
-                        for i in ['GOOG', 'AAPL', 'AMZN']],
-                    value='GOOG'
-                )
-            ]),
-            ddk.Graph(id='update-graph', style={'height':300}),
-        ]),
-    ]),
+app.layout = ddk.App(
+    [
+        ddk.Header(
+            [
+                ddk.Logo(src=app.get_asset_url("logo.png")),
+                ddk.Title("Dash Enterprise Sample Application!!"),
+            ]
+        ),
+        ddk.Row(
+            children=[
+                ddk.Card(
+                    children=[
+                        ddk.CardHeader(
+                            children=[
+                                dcc.Dropdown(
+                                    id="title-dropdown",
+                                    options=[
+                                        {"label": i, "value": i}
+                                        for i in ["GOOG", "AAPL", "AMZN"]
+                                    ],
+                                    value="GOOG",
+                                )
+                            ]
+                        ),
+                        ddk.Graph(id="update-graph", style={"height": 300}),
+                    ]
+                ),
+            ]
+        ),
+        ddk.Row(
+            children=[
+                ddk.Card(
+                    width=50,
+                    children=ddk.Graph(
+                        figure=px.line(
+                            df, x="date", y=["AMZN", "FB"], title="Stock Prices"
+                        )
+                    ),
+                ),
+                ddk.Card(
+                    width=50,
+                    children=ddk.Graph(
+                        figure=px.line(
+                            df, x="date", y=["AAPL", "MSFT"], title="Stock Prices"
+                        )
+                    ),
+                ),
+            ]
+        ),
+    ]
+)
 
-    ddk.Row(children=[
-        ddk.Card(width=50, children=ddk.Graph(figure=px.line(df, x="date", y=["AMZN", "FB"], title='Stock Prices'))),
 
-        ddk.Card(width=50, children=ddk.Graph(figure=px.line(df, x="date", y=["AAPL", "MSFT"], title='Stock Prices')))
-    ])
-])
-
-
-@app.callback(Output('update-graph', 'figure'),
-              [Input('title-dropdown', 'value')])
+@app.callback(Output("update-graph", "figure"), [Input("title-dropdown", "value")])
 def update_graph(value):
-    if value == 'GOOG':
-        return px.line(df, x="date", y="GOOG", title='Google Stock Price')
-    elif value == 'AAPL':
-        return px.line(df, x="date", y="AAPL", title='Apple Stock Price')
-    elif value == 'AMZN':
-        return px.line(df, x="date", y="AMZN", title='Amazon Stock Price')
+    if value == "GOOG":
+        return px.line(df, x="date", y="GOOG", title="Google Stock Price")
+    elif value == "AAPL":
+        return px.line(df, x="date", y="AAPL", title="Apple Stock Price")
+    elif value == "AMZN":
+        return px.line(df, x="date", y="AMZN", title="Amazon Stock Price")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
